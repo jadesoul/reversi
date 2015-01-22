b8b6857656e9d3c3723845293fe5583ea572b2d6
Shenglan Wu <wslgb2006@gmail.com>
2015-01-22 10:02:20 +0800

    ad

---
+++ src/engine/hash.h
@@ -0,0 +1,19 @@
+#ifndef HASH_H_1421891912_21
+#define HASH_H_1421891912_21
+/**
+ * File: hash.h
+ * Description: 
+ *
+ * Copyright (c) 2010-2013 Jadesoul (Home Page: http://jadesoul.sinaapp.com) <shenglan.wsl@alibaba-inc.com>
+ * 
+ * Date: 2015-01-22 09:58:32.207848
+ * Written In: Alibaba-inc, Hangzhou, China
+ */
+
+#include <pypp.h>
+
+int main () {
+	cout<<"hello, world"<<endl;
+}
+
+#endif /* HASH_H_1421891912_21 */
---
+++ src/engine/state.h
@@ -0,0 +1,19 @@
+#ifndef STATE_H_1421891892_98
+#define STATE_H_1421891892_98
+/**
+ * File: state.h
+ * Description: 
+ *
+ * Copyright (c) 2010-2013 Jadesoul (Home Page: http://jadesoul.sinaapp.com) <shenglan.wsl@alibaba-inc.com>
+ * 
+ * Date: 2015-01-22 09:58:12.976126
+ * Written In: Alibaba-inc, Hangzhou, China
+ */
+
+#include <pypp.h>
+
+int main () {
+	cout<<"hello, world"<<endl;
+}
+
+#endif /* STATE_H_1421891892_98 */
